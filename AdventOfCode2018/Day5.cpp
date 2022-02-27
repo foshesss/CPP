04576ae45167c54bb802dd6e794f4b0e75a71186
foshesss <foshessss@gmail.com>
2022-02-27 03:06:33 -0500

    day5 solved :yawn:

--- AdventOfCode2018/Day5.cpp
+++ AdventOfCode2018/Day5.cpp
@@ -32,7 +32,55 @@ int Day5P1() {
     return line.length();
 }
 
+int Day5P2() {
+    string line;
+    ifstream file(input);
+
+    getline(file, line);
+
+    int smallest = 0;
+
+    for (int _ = 65; _ < 97; _++) {
+        string n = "";
+
+        for (int j = 0; j < line.length(); j++) {
+            int val = int(line[j]);
+
+            if (val != _ && val - 32 != _) {
+                n += line[j];
+            }
+        }
+
+        int prev = int(n[0]);
+        int i = 0;
+
+        while (i != n.length()) {
+            int val = int(n[i]);
+
+            if (abs(val - prev) == 32) {
+                string sub = n.substr(i - 1, 2);
+                n = n.substr(0, i - 1) + n.substr(i + 1);
+
+                i -= 2;
+                prev = int(n[i]);
+                continue;
+            }
+
+            prev = val;
+            i++;
+        }
+
+        int len = n.length();
+
+        if (smallest == 0 || len < smallest) {
+            smallest = len;
+        }
+    }
+
+    return smallest;
+}
+
 int main() {
-    cout << Day5P1() << endl;
+    cout << Day5P2() << endl;
     return 0;
 }
