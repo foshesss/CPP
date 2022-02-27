e96c24cb5a1895568afce014a93b481874f4e139
foshesss <foshessss@gmail.com>
2022-02-27 02:52:44 -0500

    I'm working on 2018, not 2019. Also D5P1 solved.

--- AdventOfCode2018/Day5.cpp
+++ AdventOfCode2018/Day5.cpp
@@ -5,33 +5,31 @@ using namespace std;
 
 string input = "Inputs/Day5_input.txt";
 
-string Day5P1() {
+int Day5P1() {
     string line;
     ifstream file(input);
 
     getline(file, line);
 
     int prev = int(line[0]);
-    // previous:
-    for (int i = 1; i < line.length(); i++) {
+    int i = 0;
+    while (i != line.length()) {
         int val = int(line[i]);
 
         if (abs(val - prev) == 32) {
-            // remove these 2
             string sub = line.substr(i - 1, 2);
-            cout << "SUBSTRING: " << sub << endl;
+            line = line.substr(0, i - 1) + line.substr(i + 1);
 
-            line = line.substr(0, i - 1) + line.substr(i + 2, line.length());
-
-            cout << line << endl;
-            prev = -5000;
+            i -= 2;
+            prev = int(line[i]);
             continue;
         }
 
         prev = val;
+        i++;
     }
 
-    return line;
+    return line.length();
 }
 
 int main() {
